ef86ba39eab7c67a5d71c6c92bb57e405c4daffd
Martijn van de Rijdt <martijn@enketo.org>
2016-12-23 11:30:28 -0700

    added: click on header updates URL hash for easy URL sharing

--- js/script.js
+++ js/script.js
@@ -1,54 +1,87 @@
 document.addEventListener('DOMContentLoaded', function() {
-	addNavMenu();
-	addBadges();
+    addNavMenu();
+    setHeaderListeners();
 });
 
-// adds Navigation Menu up to 2 levels deep
+/**
+ * Adds listeners to a NodeList.
+ * 
+ * @param {NodeList} elements list of HTML Elements
+ * @param {String} event type
+ */
+function addListeners(elements, event, handler) {
+    if (elements && elements.length) {
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener(event, handler);
+        }
+    }
+}
+
+/**
+ * Adds Navigation Menu up to 2 levels deep
+ */
 function addNavMenu() {
-	var navListEl = document.createElement('ul'),
-		sectionHeadings = document.querySelectorAll('h2[id]');
+    var navListEl = document.createElement('ul'),
+        sectionHeadings = document.querySelectorAll('h2[id]');
 
-	for (var i = 0; i < sectionHeadings.length; ++i) {
-		var subHeadings = sectionHeadings[i].parentNode.querySelectorAll('h3[id]'),
-			navSectionEl = navListEl.appendChild(getMenuElement(sectionHeadings[i]));
+    for (var i = 0; i < sectionHeadings.length; ++i) {
+        var subHeadings = sectionHeadings[i].parentNode.querySelectorAll('h3[id]'),
+            navSectionEl = navListEl.appendChild(getMenuElement(sectionHeadings[i]));
 
-		if (subHeadings.length > 0) {
-			var navSubSectionListEl = navSectionEl.appendChild(document.createElement('ul'));
-			for (var j = 0; j < subHeadings.length; ++j) {
-				navSubSectionListEl.appendChild(getMenuElement(subHeadings[j]));
-			}
-		}
-	}
+        if (subHeadings.length > 0) {
+            var navSubSectionListEl = navSectionEl.appendChild(document.createElement('ul'));
+            for (var j = 0; j < subHeadings.length; ++j) {
+                navSubSectionListEl.appendChild(getMenuElement(subHeadings[j]));
+            }
+        }
+    }
 
-	document.querySelector('.sidenav').appendChild(navListEl);
+    document.querySelector('.sidenav').appendChild(navListEl);
 }
 
-// creates a menu element from a heading element
+/**
+ * Creates a menu element from a heading element
+ * @param  {Element} headingEl a heading element
+ */
 function getMenuElement(headingEl) {
-	var listItemEl = document.createElement('li'),
-		linkEl = document.createElement('a');
+    var listItemEl = document.createElement('li'),
+        linkEl = document.createElement('a');
 
-	linkEl.setAttribute('href', '#' + headingEl.textContent.toLowerCase().replace(/\s/g, '-'));
-	linkEl.appendChild(document.createTextNode(headingEl.textContent));
+    linkEl.setAttribute('href', '#' + headingEl.textContent.toLowerCase().replace(/\s/g, '-'));
+    linkEl.appendChild(document.createTextNode(headingEl.textContent));
 
-	listItemEl.appendChild(linkEl);
+    listItemEl.appendChild(linkEl);
 
-	return listItemEl;
+    return listItemEl;
 }
 
-// add badges from review and pending links
-function addBadges() {
-	var link, clss,
-		links = document.querySelectorAll('a');
+/**
+ * Add listeners for header clicks to update URL #hash
+ */
+function setHeaderListeners() {
+    var headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+    addListeners(headers, 'click', headerClickHandler);
+}
 
-	for (var i = 0; i < links.length; ++i) {
-		link = links[i];
-		if (link.textContent === 'review' || link.textContent === 'pending') {
-			link.classList.add('badge');
-			link.classList.add(link.textContent);
-			if (link.getAttribute('href') === '') {
-				link.setAttribute('href', '#');
-			}
-		}
-	}
+/** 
+ * Looks two levels up for an id element and changes URL hash if present.
+ * 
+ * @param  {Event]} event
+ */
+function headerClickHandler(event) {
+    var target = event.currentTarget;
+    if (target) {
+        if (target.id) {
+            setHash(target.id);
+        }
+    }
+}
+
+/**
+ * Sets hash in URL without triggering scrolling. Adds state to history.
+ * @param {string} hash [description]
+ */
+function setHash(hash) {
+    hash = (hash && hash.indexOf('#') !== 0) ? '#' + hash : hash;
+    history.pushState({}, '', hash);
 }
