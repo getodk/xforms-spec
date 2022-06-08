6f64382cfa3c7de9d438104a62090470c86e7178
eyelidlessness <gnosis@gmail.com>
2022-06-08 12:49:10 -0700

    Events & actions: organize all examples together, include setgeopoint examples

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ function getMenuElement(headingEl) {
     var listItemEl = document.createElement('li'),
         linkEl = document.createElement('a');
 
-    linkEl.setAttribute('href', '#' + headingEl.textContent.toLowerCase().replace(/\s/g, '-').replace(/,/, ''));
+    linkEl.setAttribute('href', '#' + headingEl.textContent.toLowerCase().replace(/\s/g, '-').replace(/[,'’]/, ''));
     linkEl.appendChild(document.createTextNode(headingEl.textContent));
 
     listItemEl.appendChild(linkEl);
