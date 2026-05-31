9afdc6bb1d420c6aa7730f3e1a191f12ec46112c
alxndrsn <alxndrsn>
2026-05-31 07:50:33 +0000

    build-args correctly

--- dev-docker.sh
+++ dev-docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eu
 set -o pipefail
 
-docker build .
-docker run -it --rm --volume $PWD:/work -p 4000:4000 "$(docker build --build-arg FROZEN_MODE=false --quiet .)"
+build_image() {
+  docker build --build-arg FROZEN_MODE=false .
+}
+
+build_image
+docker run -it --rm --volume $PWD:/work -p 4000:4000 "$(build_image --quiet)"
