7b798b29760cb7c9c4187ec70f5d42464c97f181
alxndrsn <alxndrsn>
2026-06-02 08:30:35 +0000

    use exec

--- dev-docker.sh
+++ dev-docker.sh
@@ -4,4 +4,4 @@ set -o pipefail
 tag=odk-xform-spec-dev
 
 docker build -t "$tag" .
-docker run -it --rm -v "$PWD:/work" -p 4000:4000 "$tag"
+exec docker run -it --rm -v "$PWD:/work" -p 4000:4000 "$tag"
