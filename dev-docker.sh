403fbc151331c87acedc72eb95380402a865bef3
alxndrsn <alxndrsn>
2026-06-02 08:29:26 +0000

    define tag var

--- dev-docker.sh
+++ dev-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eu
 set -o pipefail
 
-docker build -t odk-xform-spec-dev .
-docker run -it --rm -v "$PWD:/work" -p 4000:4000 odk-xform-spec-dev
+tag=odk-xform-spec-dev
+
+docker build -t "$tag" .
+docker run -it --rm -v "$PWD:/work" -p 4000:4000 "$tag"
