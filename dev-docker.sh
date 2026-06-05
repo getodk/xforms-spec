03c8e7c6942337fd0bceed4fecc39b3f15fa1e49
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2026-06-05 11:02:11 +0300

    dev-docker: tighten docker network binding (#366)

--- dev-docker.sh
+++ dev-docker.sh
@@ -4,4 +4,4 @@ set -o pipefail
 tag=odk-xform-spec-dev
 
 docker build -t "$tag" --build-arg FROZEN_MODE=false .
-exec docker run -it --rm --init -v "$(pwd):/work" -p 4000:4000 "$tag"
+exec docker run -it --rm --init -v "$(pwd):/work" -p 127.0.0.1:4000:4000 "$tag"
