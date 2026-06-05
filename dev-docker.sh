6204143a68e95f623a9dae9429152bcadf038aa6
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2026-06-05 10:58:54 +0300

    ci: build pull requests (#360)
    Check the site builds before merging to mainline (`gh-pages`).

--- dev-docker.sh
+++ dev-docker.sh
@@ -3,5 +3,5 @@ set -o pipefail
 
 tag=odk-xform-spec-dev
 
-docker build -t "$tag" .
+docker build -t "$tag" --build-arg FROZEN_MODE=false .
 exec docker run -it --rm --init -v "$(pwd):/work" -p 4000:4000 "$tag"
